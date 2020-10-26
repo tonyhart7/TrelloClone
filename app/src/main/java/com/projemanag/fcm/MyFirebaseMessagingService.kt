fa4415c86240a5bdd482c8e14f0330510324f658
tonyhart7 <tonyhart2020@gmail.com>
2020-10-26 22:52:52 +0700

    initial commit

---
+++ app/src/main/java/com/projemanag/fcm/MyFirebaseMessagingService.kt
@@ -0,0 +1,82 @@
+package com.projemanag.fcm
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.projemanag.R
+import com.projemanag.activity.MainActivity
+import com.projemanag.activity.SingInActivity
+import com.projemanag.firebase.FireStore
+import com.projemanag.utils.Constants
+
+class MyFirebaseMessagingService: FirebaseMessagingService() {
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+
+        Log.d(TAG, "${remoteMessage.from}")
+
+        remoteMessage.data.isNotEmpty().let {
+            Log.d(TAG, "Message Data Payload: ${remoteMessage.data}")
+
+            val title = remoteMessage.data[Constants.FCM_KEY_TITLE]!!
+            val message = remoteMessage.data[Constants.FCM_KEY_MESSAGE]!!
+
+            sendNotification(title, message)
+        }
+
+        remoteMessage.notification?.let {
+            Log.d(TAG, "Message Notification Body: ${it.body}")
+        }
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+        Log.e(TAG,"Refreshed Token: $token")
+        sendRegistrationToServer(token)
+    }
+
+    private fun sendRegistrationToServer(token: String? ){
+        //TODO
+    }
+
+    private fun sendNotification(title: String, message: String){
+
+
+        val intent = if (FireStore().getCurrentUserId().isNotEmpty()){
+            Intent(this, MainActivity::class.java)
+        }else{
+            Intent(this, SingInActivity::class.java)
+        }
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0,intent, PendingIntent.FLAG_ONE_SHOT)
+        val channelId = this.resources.getString(R.string.default_notification_channel_id)
+        val defaultSoundURI = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(
+            this, channelId
+        ).setSmallIcon(R.drawable.ic_stat_ic_notification).setContentTitle(title)
+            .setContentText(message).setAutoCancel(true).setSound(defaultSoundURI)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val channel = NotificationChannel(channelId, "Channel Manage Project", NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(0, notificationBuilder.build())
+    }
+
+    companion object {
+        private const val TAG = "MyFirebaseMsgService"
+    }
+}
